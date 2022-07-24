13795f3a52289db67c973f3861790bf4a413c993
visavin <visavin@yandex.ru>
2022-07-24 20:36:19 +0300

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,28 +9,23 @@ import Swiper, { Navigation, Pagination } from 'swiper';
   const overlay = document.querySelector('.overlay')
   const menu = document.querySelector('.mobile-menu')
 
-  // window.addEventListener('resize', () => {
-  //   let vh = window.innerHeight * 0.01;
-  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // });
-
   function doAction (action) {
     switch (action) {
       case 'open menu':
         menu.classList.add('mobile-menu--active')
         overlay.classList.add('overlay--active')
-        // document.body.style.overflow= 'hidden';
+        document.body.style.overflow= 'hidden';
         return
       case 'close menu':
         menu.classList.remove('mobile-menu--active')
         overlay.classList.remove('overlay--active')
-        // document.body.style.overflow = '';
+        document.body.style.overflow = '';
         return
       default:
         menu.classList.remove('mobile-menu--active')
         // modal.classList.remove('modal--active')
         overlay.classList.remove('overlay--active')
-        // document.body.style.overflow = '';
+        document.body.style.overflow = '';
     }
   }
 
@@ -46,37 +41,78 @@ import Swiper, { Navigation, Pagination } from 'swiper';
     }
   }
 
-  const dropDownButton = document.querySelector('.repair-brands__read-more')
-  const repairBrandsSwiper = document.querySelector('.repair-brands__swiper')
-  const repairBrands = document.querySelector('.repair-brands')
-  const dropdownText = document.querySelector('.repair-brands__read-more .dropdown__text')
-  const dropdownIcon = document.querySelector('.repair-brands__read-more .dropdown__icon')
+  const dropdownButton = document.querySelectorAll('.dropdown')
+  const dropdownBlock = [
+    document.querySelector('.about-text__text'),
+    document.querySelector('.repair-brands__swiper')
+  ]
+  const DropdownText = [
+    document.querySelector('.about-text__button .dropdown__text'),
+    document.querySelector('.repair-brands__read-more .dropdown__text')
+  ]
+  const DropdownIcon = [
+    document.querySelector('.about-text__button .dropdown__icon'),
+    document.querySelector('.repair-brands__read-more .dropdown__icon')
+  ]
+  const baseBlockHeight = [
+    dropdownBlock[0].scrollHeight + 'px',
+    dropdownBlock[1].scrollHeight + 'px'
+  ]
 
-  dropDownButton.onclick = function () {
-    const isDropped = this.classList.toggle('dropped')
-    if (isDropped) {
-      repairBrandsSwiper.style.height = '296px'
-      repairBrands.style.paddingBottom = '14px'
-      dropdownText.textContent = 'Скрыть'
-      dropdownIcon.style.transform = 'rotate(180deg)';
-    } else {
-      repairBrandsSwiper.style.height = '200px'
-      repairBrands.style.paddingBottom = '32px'
-      dropdownText.textContent = 'Показать все'
-      dropdownIcon.style.transform = 'none';
+  const hiddenBlockHeight = []
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    hiddenBlockHeight[0] = '160px'
+  } else {
+    hiddenBlockHeight[0] = '90px'
+  }
+  hiddenBlockHeight[1] = '200px'
+
+  for (let i = 0; i < dropdownButton.length; i++) {
+    toggleHeight(
+      dropdownButton[i],
+      dropdownBlock[i],
+      baseBlockHeight[i],
+      hiddenBlockHeight[i],
+      DropdownText[i],
+      DropdownIcon[i]
+    )
+  }
+
+  function toggleHeight(button, block, blockHeight, hiddenBlockHeight, DropdownText, DropdownIcon) {
+    const innerText = DropdownText.textContent
+    // button.addEventListener('click', (ev) => {
+    //   const isDropped = ev.target.classList.toggle('dropped')
+    button.onclick = function () {
+      const isDropped = this.classList.toggle('dropped')
+      if (isDropped) {
+        block.style.height = blockHeight
+        DropdownText.textContent = 'Скрыть'
+        DropdownIcon.style.transform = 'rotate(180deg)';
+      } else {
+        block.style.height = hiddenBlockHeight
+        DropdownText.textContent = innerText
+        DropdownIcon.style.transform = 'none';
+      }
     }
   }
 
+  const repairBrandsDropDownButton = document.querySelector('.repair-brands__read-more')
+  const repairBrandsSwiper = document.querySelector('.repair-brands__swiper')
+  const repairBrandsDropdownText = document.querySelector('.repair-brands__read-more .dropdown__text')
+  const repairBrandsDropdownIcon = document.querySelector('.repair-brands__read-more .dropdown__icon')
+
+  const repairBrands = document.querySelector('.repair-brands')
   const breakpoint = window.matchMedia('(max-width:767px)')
   let mySwiper
   const breakpointChecker = function () {
     if (breakpoint.matches === false) {
       repairBrandsSwiper.style.height = '200px'
       repairBrands.style.paddingBottom = '32px'
-      dropdownText.textContent = 'Показать все'
+      repairBrandsDropdownText.textContent = 'Показать все'
+      repairBrandsDropdownIcon.style.transform = 'none';
       if (mySwiper !== undefined) mySwiper.destroy(true, true)
     } else if (breakpoint.matches === true) {
-      dropDownButton.classList.remove('dropped')
+      repairBrandsDropDownButton.classList.remove('dropped')
       repairBrandsSwiper.style.height = '140px'
       repairBrands.style.paddingBottom = '0px'
       return enableSwiper()
