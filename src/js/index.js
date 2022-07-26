81843a653fadc63b45ae7d05169f00685ba6ee38
visavin <visavin@yandex.ru>
2022-07-27 00:19:15 +0300

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../scss/style.scss';
-
+import { disableScroll, enableScroll } from './scroll.js'
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 (function () {
@@ -17,14 +17,12 @@ import Swiper, { Navigation, Pagination } from 'swiper';
       case 'open menu':
         menu.classList.add('mobile-menu--active')
         overlay.classList.add('overlay--active')
-        document.body.style.overflow= 'hidden';
-        menu.style.overflow= 'auto';
+        disableScroll()
         return
       case 'close menu':
         menu.classList.remove('mobile-menu--active')
         overlay.classList.remove('overlay--active')
-        document.body.style.overflow = '';
-        menu.style.overflow= '';
+        enableScroll()
         return
       case 'open modal':
         if (type === 'request call') {
@@ -40,22 +38,18 @@ import Swiper, { Navigation, Pagination } from 'swiper';
         menu.classList.remove('mobile-menu--active')
         modal.classList.add('modal--active')
         overlay.classList.add('overlay--active')
-        modal.style.overflow= 'auto';
-        document.body.style.overflow= 'hidden';
+        disableScroll()
         return
       case 'close modal':
         modal.classList.remove('modal--active')
         overlay.classList.remove('overlay--active')
-        document.body.style.overflow = '';
-        modal.style.overflow= '';
+        enableScroll()
         return
       default:
         menu.classList.remove('mobile-menu--active')
         modal.classList.remove('modal--active')
         overlay.classList.remove('overlay--active')
-        document.body.style.overflow = '';
-        menu.style.overflow= '';
-        modal.style.overflow= '';
+        enableScroll()
     }
   }
 
@@ -72,21 +66,30 @@ import Swiper, { Navigation, Pagination } from 'swiper';
   }
 
   const dropdownButton = document.querySelectorAll('.dropdown')
+  const brandsDropdownText = document.querySelector('.brands__read-more .dropdown__text')
+  const brandsDropdownIcon = document.querySelector('.brands__read-more .dropdown__icon')
+  const technicDropdownText = document.querySelector('.technic__read-more .dropdown__text')
+  const technicDropdownIcon = document.querySelector('.technic__read-more .dropdown__icon')
+
   const dropdownBlock = [
     document.querySelector('.about-text__text'),
-    document.querySelector('.repair-brands__swiper')
+    document.querySelector('.brands__swiper'),
+    document.querySelector('.technic__swiper')
   ]
   const DropdownText = [
     document.querySelector('.about-text__button .dropdown__text'),
-    document.querySelector('.repair-brands__read-more .dropdown__text')
+    brandsDropdownText,
+    technicDropdownText
   ]
   const DropdownIcon = [
     document.querySelector('.about-text__button .dropdown__icon'),
-    document.querySelector('.repair-brands__read-more .dropdown__icon')
+    brandsDropdownIcon,
+    technicDropdownIcon
   ]
   const baseBlockHeight = [
     dropdownBlock[0].scrollHeight + 'px',
-    dropdownBlock[1].scrollHeight + 'px'
+    dropdownBlock[1].scrollHeight + 'px',
+    dropdownBlock[2].scrollHeight + 'px'
   ]
 
   const hiddenBlockHeight = []
@@ -96,6 +99,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
     hiddenBlockHeight[0] = '90px'
   }
   hiddenBlockHeight[1] = '200px'
+  hiddenBlockHeight[2] = '200px'
 
   for (let i = 0; i < dropdownButton.length; i++) {
     toggleHeight(
@@ -108,43 +112,49 @@ import Swiper, { Navigation, Pagination } from 'swiper';
     )
   }
 
-  function toggleHeight(button, block, blockHeight, hiddenBlockHeight, DropdownText, DropdownIcon) {
-    const innerText = DropdownText.textContent
+  function toggleHeight(button, block, blockHeight, hiddenBlockHeight, dropdownText, dropdownIcon) {
+    const innerText = dropdownText.textContent
     // button.addEventListener('click', (ev) => {
     //   const isDropped = ev.target.classList.toggle('dropped')
     button.onclick = function () {
       const isDropped = this.classList.toggle('dropped')
       if (isDropped) {
         block.style.height = blockHeight
-        DropdownText.textContent = 'Скрыть'
-        DropdownIcon.style.transform = 'rotate(180deg)';
+        dropdownText.textContent = 'Скрыть'
+        dropdownIcon.style.transform = 'rotate(180deg)';
       } else {
         block.style.height = hiddenBlockHeight
-        DropdownText.textContent = innerText
-        DropdownIcon.style.transform = 'none';
+        dropdownText.textContent = innerText
+        dropdownIcon.style.transform = 'none';
       }
     }
   }
 
-  const repairBrandsDropDownButton = document.querySelector('.repair-brands__read-more')
-  const repairBrandsSwiper = document.querySelector('.repair-brands__swiper')
-  const repairBrandsDropdownText = document.querySelector('.repair-brands__read-more .dropdown__text')
-  const repairBrandsDropdownIcon = document.querySelector('.repair-brands__read-more .dropdown__icon')
+  const brandsDropDownButton = document.querySelector('.brands__read-more')
+  const brandsSwiper = document.querySelector('.brands__swiper')
+  const technicDropDownButton = document.querySelector('.technic__read-more')
+  const technicSwiper = document.querySelector('.technic__swiper')
 
-  const repairBrands = document.querySelector('.repair-brands')
+
+  const repair = document.querySelector('.repair')
   const breakpoint = window.matchMedia('(max-width:767px)')
   let mySwiper
   const breakpointChecker = function () {
     if (breakpoint.matches === false) {
-      repairBrandsSwiper.style.height = '200px'
-      repairBrands.style.paddingBottom = '32px'
-      repairBrandsDropdownText.textContent = 'Показать все'
-      repairBrandsDropdownIcon.style.transform = 'none';
+      brandsSwiper.style.height = '200px'
+      technicSwiper.style.height = '200px'
+      repair.style.paddingBottom = ''
+      brandsDropdownText.textContent = 'Показать все'
+      brandsDropdownIcon.style.transform = 'none';
+      technicDropdownText.textContent = 'Показать все'
+      technicDropdownIcon.style.transform = 'none';
       if (mySwiper !== undefined) mySwiper.destroy(true, true)
     } else if (breakpoint.matches === true) {
-      repairBrandsDropDownButton.classList.remove('dropped')
-      repairBrandsSwiper.style.height = '140px'
-      repairBrands.style.paddingBottom = '0px'
+      brandsDropDownButton.classList.remove('dropped')
+      technicDropDownButton.classList.remove('dropped')
+      brandsSwiper.style.height = '140px'
+      technicSwiper.style.height = '228px'
+      repair.style.paddingBottom = '0px'
       return enableSwiper()
     }
   }
